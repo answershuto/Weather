dad6adddc0074f2a926243a7c115d0d9b78ca0c7
answershuto <905307789@qq.com>
2016-09-21 23:28:43 +0800

    用todo移植实现前端redux基础架构

---
+++ views/containers/App.js
@@ -0,0 +1,43 @@
+import React, { Component, PropTypes } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import Header from '../components/Header'
+import MainSection from '../components/MainSection'
+import * as TodoActions from '../actions/todos'
+
+class App extends Component {
+  render() {
+    const { todos, actions } = this.props
+    console.log(todos)
+    console.log(actions)
+    return (
+      <div>
+        <Header addTodo={actions.addTodo} />
+        <MainSection todos={todos} actions={actions} />
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  todos: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+}
+
+function mapStateToProps(state) {
+  return {
+    todos: state.todos
+    //todos: [{completed:false,id:0,text:"s"}]
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(TodoActions, dispatch)
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
