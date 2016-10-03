8600317f065a0f77f0b6a1f4118eda3d95ede4db
answershuto <905307789@qq.com>
2016-10-04 00:01:50 +0800

    未来六天天气显示图片

--- views/components/Weather.react.js
+++ views/components/Weather.react.js
@@ -14,6 +14,72 @@ const bgImgs = {
 	'风': 'bg12.gif',
 }
 
+ //定义天气类型 
+const weatherArr={
+ 	"00":"晴",
+ 	"01":"多云",
+ 	"02":"阴",
+ 	"03":"阵雨",
+ 	"04":"雷阵雨",
+ 	"05":"雷阵雨伴有冰雹",
+ 	"06":"雨夹雪",
+ 	"07":"小雨",
+ 	"08":"中雨",
+ 	"09":"大雨",
+ 	"10":"暴雨",
+ 	"11":"大暴雨",
+ 	"12":"特大暴雨"
+ 	,"13":"阵雪",
+ 	"14":"小雪",
+ 	"15":"中雪",
+ 	"16":"大雪",
+ 	"17":"暴雪",
+ 	"18":"雾",
+ 	"19":"冻雨",
+ 	"20":"沙尘暴",
+ 	"21":"小到中雨",
+ 	"22":"中到大雨",
+ 	"23":"大到暴雨",
+ 	"24":"暴雨到大暴雨",
+ 	"25":"大暴雨到特大暴雨",
+ 	"26":"小到中雪",
+ 	"27":"中到大雪",
+ 	"28":"大到暴雪",
+ 	"29":"浮尘",
+ 	"30":"扬沙",
+ 	"31":"强沙尘暴",
+ 	"53":"霾",
+ 	"99":""
+ }; 
+
+ //定义风向数组 
+const fxArr={
+ 	"0":"无持续风向",
+ 	"1":"东北风",
+ 	"2":"东风",
+ 	"3":"东南风",
+ 	"4":"南风",
+ 	"5":"西南风",
+ 	"6":"西风",
+ 	"7":"西北风",
+ 	"8":"北风",
+ 	"9":"旋转风"
+ }; 
+
+ //定义风力数组 
+ const flArr={
+ 	"0":"微风",
+ 	"1":"3-4级",
+ 	"2":"4-5级",
+ 	"3":"5-6级",
+ 	"4":"6-7级",
+ 	"5":"7-8级",
+ 	"6":"8-9级",
+ 	"7":"9-10级",
+ 	"8":"10-11级",
+ 	"9":"11-12级"
+ }; 
+
 class Weather extends Component{
 	constructor(props){
 		super(props);
@@ -34,8 +100,6 @@ class Weather extends Component{
 	render(){
 		const { weather } = this.state;
 
-		//console.log(weather)
-
 		let info = weather.weatherinfo;
 
 		let bg = 'bg5.gif';
@@ -52,7 +116,32 @@ class Weather extends Component{
 		let FutureWeathers = [];
 		if (this.state.showImageState) {
 			if (weather.futureWeather.f) {
-				FutureWeathers.push(<canvas id="myCanvas" className="ui-weather-canvas" key='0'></canvas>);
+				let arr = [],arr2 = [];
+				for(let i=0;i<6;i++){
+					arr.push(<div key={i} className="col-md-2 col-xs-2">
+						<div>
+							<img src={"http://m.weather.com.cn/img/c"+Number(weather.futureWeather.f.f1[i].fa)+".gif"}></img>
+						</div>
+					</div>);
+					arr2.push(<div key={i} className="col-md-2 col-xs-2">
+						<div>
+							<img src={"http://m.weather.com.cn/img/c"+Number(weather.futureWeather.f.f1[i].fb)+".gif"}></img>
+						</div>
+					</div>);
+				}
+				FutureWeathers = <div>
+					<div className="container">
+						<div className="row">
+							{arr}
+						</div>
+					</div>
+					<canvas id="myCanvas" className="ui-weather-canvas" key='0'></canvas>
+					<div className="container">
+						<div className="row">
+							{arr2}
+						</div>
+					</div>
+				</div>
 			};
 		}
 		else{
@@ -112,8 +201,6 @@ class Weather extends Component{
 				ctx.lineWidth = 1;//线宽
 				ctx.strokeText(info[i].fc+'℃',w-10,h-10);
 			}
-
-		  	
 		};
 	}
 }
